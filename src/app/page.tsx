345bef17513d61bd67524284dd86653747d41087
dhruvpatel99269 <99269dhruvpatel@gmail.com>
2024-05-04 15:29:34 +0530

    landing page font changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,8 +54,8 @@ export default function Home() {
         <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
           <div className="mb-12 px-6 lg:px-8 ">
             <div className="mx-auto max-w-2xl sm:text-center">
-              <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">Register in minutes!</h2>
-              <p className="mt-4 text-lg text-gray-600">Registering have never been easier than with <span className="font-semibold">symbiSoc</span></p>
+              <h2 className="mt-2 font-bold text-4xl text-gray-200 sm:text-5xl">Register in minutes!</h2>
+              <p className="mt-4 text-lg text-zinc-400">Registering have never been easier than with <span className="font-semibold">symbiSoc</span></p>
             </div>
           </div>
 
@@ -65,21 +65,21 @@ export default function Home() {
               <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
                 <span className="text-sm font-medium text-violet-600">Step 1</span>
                 <span className="text-xl font-semibold">Create an account</span>
-                <span className="mt-2 text-zinc-700">Create an account using your college email and password and sign-in.</span>
+                <span className="mt-2 text-zinc-400">Create an account using your college email and password and sign-in.</span>
               </div>
             </li>
             <li className="md:flex-1">
               <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
                 <span className="text-sm font-medium text-violet-600">Step 2</span>
                 <span className="text-xl font-semibold">Fill up the form</span>
-                <span className="mt-2 text-zinc-700">Click on the ongoing events and fill up the form for the event you want to register for.</span>
+                <span className="mt-2 text-zinc-400">Click on the ongoing events and fill up the form for the event you want to register for.</span>
               </div>
             </li>
             <li className="md:flex-1">
               <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
                 <span className="text-sm font-medium text-violet-600">Step 3</span>
                 <span className="text-xl font-semibold">Enjoy your event</span>
-                <span className="mt-2 text-zinc-700">It&apos;s that simple!</span>
+                <span className="mt-2 text-zinc-400">It&apos;s that simple!</span>
               </div>
             </li>
           </ol>
--- src/components/CardContainer/Codex.tsx
+++ src/components/CardContainer/Codex.tsx
@@ -9,7 +9,7 @@ const Codex = () => {
             <CardBody className="bg-gradient-to-tl from-orange-100 via-red-100 to-orange-100 hover:bg-gradient-to-tl hover:from-red-200 hover:via-orange-100 hover:to-red-200 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
                 <CardItem
                 translateZ="50"
-                className="text-xl font-bold text-neutral-600 dark:text-white"
+                className="text-xl font-bold text-neutral-600"
                 >
                 CODEX(SIT&apos;s Coding Club)
                 </CardItem>
@@ -25,7 +25,7 @@ const Codex = () => {
                 <CardItem
                 as="p"
                 translateZ="60"
-                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                className="text-neutral-500 text-sm max-w-sm mt-2"
                 >
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, nisi! Blanditiis, id fugit! Consequuntur neque veniam culpa soluta, voluptates natus.
                 </CardItem>
