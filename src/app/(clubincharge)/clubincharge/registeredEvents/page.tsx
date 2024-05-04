9a81611b7e4cba58a248b431f07ec153b9de4ac2
dhruvpatel99269 <99269dhruvpatel@gmail.com>
2024-05-05 02:06:39 +0530

    no events handling

--- src/app/(clubincharge)/clubincharge/registeredEvents/page.tsx
+++ src/app/(clubincharge)/clubincharge/registeredEvents/page.tsx
@@ -43,7 +43,7 @@ export default function RegisteredEventsCard() {
     };
     return (
         <div className="w-full">
-            {events.map((event: EventProps, index) => (
+            {events.length === 0 ? (<div>You have not registered in upcoming events</div>) : (events.map((event: EventProps, index) => (
                 <Card key={index} className="flex flex-wrap bg-slate-700 text-neutral-950">
                     <div className='flex sm:flex-col md:flex-col lg:flex-row xl:flex-row w-full sm:w-full md:w-full lg:w-1/3 xl:w-1/3 justify-center items-center'>
                         <CardContent className="p-5 flex justify-center items-center">
@@ -84,7 +84,7 @@ export default function RegisteredEventsCard() {
                             </div>
                         </CardContent>
                     </div>
-                </Card>
+                </Card>)
             ))}
         </div>
     );
--- src/components/Cards/UpcomingEvents.tsx
+++ src/components/Cards/UpcomingEvents.tsx
@@ -84,7 +84,7 @@ export default function ViewEventsCard() {
     }
     return (
         <div className="w-full">
-            {events.map((event: EventProps, index) => (
+            {events.length === 0 ? (<div>No Upcoming Events</div>) : (events.map((event: EventProps, index) => (
                 <Card key={index} className="flex flex-wrap bg-slate-700 text-neutral-950">
                     <div className='flex sm:flex-col md:flex-col lg:flex-row xl:flex-row w-full sm:w-full md:w-full lg:w-1/3 xl:w-1/3 justify-center items-center'>
                         <CardContent className="p-5 flex justify-center items-center">
@@ -127,7 +127,7 @@ export default function ViewEventsCard() {
                             </div>
                         </CardContent>
                     </div>
-                </Card>
+                </Card>)
             ))}
         </div>
     );
